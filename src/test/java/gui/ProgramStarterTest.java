d7eb0319a7a2c68c9b4cdec6e52f4904fb318aed
koddas <johan@idioti.se>
2017-01-19 15:52:20 +0100

    Actually implemented the test

--- src/test/java/gui/ProgramStarterTest.java
+++ src/test/java/gui/ProgramStarterTest.java
@@ -2,13 +2,21 @@ package gui;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ProgramStarterTest {
 
+	ProgramStarter program;
+	
+	@Before
+	public void setup() {
+		program = new ProgramStarter();
+	}
+	
 	@Test
 	public void testMain() {
-		fail("Not yet implemented");
+		assertNotNull(program);
 	}
 
 }
