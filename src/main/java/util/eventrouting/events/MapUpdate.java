bce5408a5eb4ef9adca10aaf0c9fb56186988aad
koddas <johan@idioti.se>
2017-01-27 20:09:10 +0100

    Swap NewMapEvent for MapUpdate

--- src/main/java/util/eventrouting/events/MapUpdate.java
+++ src/main/java/util/eventrouting/events/MapUpdate.java
@@ -1,7 +1,22 @@
 package util.eventrouting.events;
 
+import game.Map;
 import util.eventrouting.PCGEvent;
 
+/**
+ * This event is used to post info on new maps.
+ * 
+ * @author Alexander Baldwin, Malmö University
+ * @author Johan Holmberg, Malmö University
+ */
 public class MapUpdate extends PCGEvent {
-
+	
+	/**
+	 * Creates a new event.
+	 * 
+	 * @param payload The map to be sent.
+	 */
+	public MapUpdate(Map payload) {
+		setPayload(payload);
+	}
 }
