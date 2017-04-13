d78f99ebe162565c8eff0f40fbeebb1329443ed1
Alex Baldwin <alexander.baldwin@mah.se>
2017-04-13 08:14:31 +0200

    Implemented GuardRoom pattern

--- src/main/java/finder/patterns/meso/GuardRoom.java
+++ src/main/java/finder/patterns/meso/GuardRoom.java
@@ -1,7 +1,54 @@
 package finder.patterns.meso;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
+import finder.graph.Edge;
+import finder.graph.Graph;
+import finder.graph.Node;
 import finder.patterns.CompositePattern;
+import finder.patterns.InventorialPattern;
+import finder.patterns.Pattern;
+import finder.patterns.micro.Enemy;
+import finder.patterns.micro.Room;
+import finder.patterns.micro.Treasure;
+import game.Map;
 
 public class GuardRoom extends CompositePattern {
-
+	public static List<CompositePattern> matches(Map map, Graph<Pattern> patternGraph) {
+		List<CompositePattern> guardRooms = new ArrayList<CompositePattern>();
+		
+		patternGraph.resetGraph();
+		
+		Queue<Node<Pattern>> nodeQueue = new LinkedList<Node<Pattern>>();
+		nodeQueue.add(patternGraph.getStartingPoint());
+		
+		while(!nodeQueue.isEmpty()){
+			Node<Pattern> current = nodeQueue.remove();
+			current.tryVisit();
+			if(current.getValue() instanceof Room){
+				List<InventorialPattern> containedEnemies = ((Room)current.getValue()).getContainedPatterns().stream().filter(p->{return p instanceof Enemy;}).collect(Collectors.toList());
+				if(containedEnemies.size() > 2){
+					GuardRoom g = new GuardRoom();
+					g.patterns.addAll(containedEnemies);
+					g.patterns.add(current.getValue());
+					guardRooms.add(g);
+					System.out.println("Got a guard room!");
+				}
+			}
+			nodeQueue.addAll(current.getEdges().stream().map((Edge<Pattern> e)->{
+				Node<Pattern> ret = null;
+				if(e.getNodeA() == current)
+					ret = e.getNodeB();
+				else
+					ret = e.getNodeA();
+				return ret;
+				}).filter((Node<Pattern> n)->{return !n.isVisited();}).collect(Collectors.toList()));
+		}
+		
+		return guardRooms;
+	}
 }
