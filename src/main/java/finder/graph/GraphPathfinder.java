258a3cbfedac2f474e6ee8111801fc09f684a4fc
Alex Baldwin <alexander.baldwin@mah.se>
2017-04-20 15:42:27 +0200

    WIP on DeadEnd

---
+++ src/main/java/finder/graph/GraphPathfinder.java
@@ -0,0 +1,82 @@
+package finder.graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import finder.graph.Graph;
+import finder.patterns.Pattern;
+
+public class GraphPathfinder {
+	
+	
+	private class PathNode {
+	    public Node<Pattern> graphNode;
+	    public PathNode parent;
+
+	    public PathNode(Node<Pattern> graphNode, PathNode parent)
+	    {
+	        this.graphNode = graphNode;
+	        this.parent = parent;
+	    }
+	    
+	}
+	
+    private Graph<Pattern> patternGraph;
+
+    public GraphPathfinder(Graph<Pattern> patternGraph)
+    {
+        this.patternGraph = patternGraph;
+        
+    }
+
+	//BFS
+    public List<Node<Pattern>> find(Node<Pattern> start, Node<Pattern> goal)
+    {
+    	patternGraph.resetGraph();
+    	List<Node<Pattern>> path = new ArrayList<Node<Pattern>>();
+
+        Queue<PathNode> queue = new LinkedList<PathNode>();
+        queue.add(new PathNode(start,null));
+        
+        
+        
+        while(!queue.isEmpty()){
+        	PathNode current = queue.remove();
+        	current.graphNode.tryVisit();
+        	if(current.graphNode == goal){
+        		return reconstructPath(current);
+        	}
+        	
+        	for(Edge<Pattern> e : current.graphNode.getEdges()){
+				if(e.getNodeA() == current.graphNode && !e.getNodeB().isVisited()){
+					queue.add(new PathNode(e.getNodeB(),current));
+					e.getNodeB().tryVisit();
+				} else if (e.getNodeB() == current.graphNode && !e.getNodeA().isVisited()){
+					queue.add(new PathNode(e.getNodeA(),current));
+					e.getNodeA().tryVisit();
+				}
+			}
+        }
+        
+        return path;
+        
+    }
+
+    private List<Node<Pattern>> reconstructPath(PathNode last)
+    {
+        List<Node<Pattern>> path = new ArrayList<Node<Pattern>>();
+        path.add(last.graphNode);
+
+        while(last.parent != null)
+        {
+            last = last.parent;
+            path.add(last.graphNode);
+        }
+
+        return path;
+    }
+
+}
+
--- src/main/java/finder/patterns/meso/DeadEnd.java
+++ src/main/java/finder/patterns/meso/DeadEnd.java
@@ -1,7 +1,55 @@
 package finder.patterns.meso;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import finder.graph.Edge;
+import finder.graph.Graph;
+import finder.graph.Node;
 import finder.patterns.CompositePattern;
+import finder.patterns.Pattern;
+import game.Map;
 
 public class DeadEnd extends CompositePattern {
+	
+	//TODO: NOTE, the quality of a dead end should be related to the amount of content in it.
 
+	/**
+	 * Searches a map for instances of this pattern and returns a list of found
+	 * instances.
+	 * 
+	 * @param map The map to search for patterns in.
+	 * @param boundary A boundary in which the pattern is searched for.
+	 * @return A list of found instances.
+	 */
+	public static List<CompositePattern> matches(Map map, Graph<Pattern> patternGraph) {
+		
+		//How to find dead ends:
+		//1. Find the critical path
+		//2. For each node in the critical path, perform a BFS starting at each edge connecting to a node that isn't in the critical path
+		//3. If an edge is part of a cycle, it is not part of a dead end
+		//4. If an edge is NOT part of a cycle, every node beyond it is in a dead end.
+		
+		List<CompositePattern> deadEnds = new ArrayList<>();
+		
+		return deadEnds;
+	}
+	
+	/**
+	 * Returns all the nodes with only one edge from a pattern graph (the leaves).
+	 * 
+	 * @param patternGraph The pattern graph.
+	 * @return Am ArrayList of leaves.
+	 */
+	List<Node<Pattern>> getLeaves(Graph<Pattern> patternGraph){
+		List<Node<Pattern>> leaves = new ArrayList<Node<Pattern>>();
+		for(Node<Pattern> n : patternGraph.getNodes().values()){
+			if (n.countEdges() == 1)
+				leaves.add(n);
+		}
+		return leaves;
+	}
+	
 }
