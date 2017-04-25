b173a68fce5335232e9edd85ec19d6d4d8411105
koddas <johan@idioti.se>
2017-04-25 11:30:07 +0200

    Skeleton for the interactive GUI

--- src/main/java/gui/InteractiveGUIController.java
+++ src/main/java/gui/InteractiveGUIController.java
@@ -1,5 +1,24 @@
 package gui;
 
-public class InteractiveGUIController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import util.eventrouting.Listener;
+import util.eventrouting.PCGEvent;
+
+public class InteractiveGUIController implements Initializable, Listener {
+
+	@Override
+	public void ping(PCGEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
