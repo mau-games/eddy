c3e42e1d887f84aee30c1107f7cae6fbb3dea4a6
koddas <johan@idioti.se>
2017-04-28 16:02:57 +0200

    The edit view is mostly done now.

--- src/main/java/gui/views/EditView.java
+++ src/main/java/gui/views/EditView.java
@@ -2,10 +2,16 @@ package gui.views;
 
 import java.io.IOException;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 
 public class EditView extends BorderPane {
+	
+	@FXML private Canvas centralCanvas;
 
 	public EditView() {
 		super();
@@ -19,5 +25,14 @@ public class EditView extends BorderPane {
 		} catch (IOException exception) {
 			throw new RuntimeException(exception);
 		}
+		
+		draw();
+	}
+
+	private void draw() {
+		GraphicsContext ctx = centralCanvas.getGraphicsContext2D();
+		
+		ctx.setFill(Color.RED);
+		ctx.fillRect(0, 0, 500, 500);
 	}
 }
