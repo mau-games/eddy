876362b396d103cd5beede862a391870bf8ab943
koddas <johan@idioti.se>
2017-05-03 13:40:08 +0200

    Added a thread argument to the Start event.

--- src/main/java/util/eventrouting/events/Start.java
+++ src/main/java/util/eventrouting/events/Start.java
@@ -3,5 +3,17 @@ package util.eventrouting.events;
 import util.eventrouting.PCGEvent;
 
 public class Start extends PCGEvent{
-
+	private int threads;
+	
+	public Start() {
+		threads = 1;
+	}
+	
+	public Start(int threads) {
+		this.threads = threads;
+	}
+	
+	public int getNbrOfThreads() {
+		return threads;
+	}
 }
